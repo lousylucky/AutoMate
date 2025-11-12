ef3a99ab492d004bcdee1f837b28e45fb872e8b0
GaspardCulis <gaspard@gasdev.fr>
2025-11-12 09:52:50 +0100

    chore(backend): Add deps

--- backend/shell.nix
+++ backend/shell.nix
@@ -5,5 +5,12 @@ with pkgs;
       uv
       pyright
       nixpkgs-fmt
+
+      (pkgs.python3.withPackages (python-pkgs: [
+        python-pkgs.msgpack
+        python-pkgs.numpy
+        python-pkgs.sounddevice
+        python-pkgs.websockets
+      ]))
     ];
   }
