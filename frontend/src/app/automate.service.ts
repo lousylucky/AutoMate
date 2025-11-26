1a0607d57a80be4c358b177107242f5ddf0e98c2
GaspardCulis <gaspard@gasdev.fr>
2025-11-26 09:28:27 +0100

    feat(automate): Create simple chat loop

--- frontend/src/app/automate.service.ts
+++ frontend/src/app/automate.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { STTService } from './services/speechRecognition.service';
-import { ChatService } from './services/chat.service';
+import { ChatService, ChatState } from './services/chat.service';
 import { ToolCall } from '@mistralai/mistralai/models/components';
+import { YoutubeSearchService } from './services/youtube-search.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutomateService {
 
-  constructor(private stt: STTService, private chat: ChatService) { }
+  constructor(private stt: STTService, private chat: ChatService, private youtube: YoutubeSearchService) { }
 
   async handleAudioCommand(audio: Blob) {
     console.log("Handle record finish")
@@ -16,7 +17,13 @@ export class AutomateService {
     let commandText = sttResponse!.text;
     console.log(`Transcribed command: ${commandText}`);
 
-    let [chatResponse, context] = await this.chat.ask([{ role: "user", content: commandText }]);
+    this.chatLoop(this.chat.buildChatState(commandText));
+
+
+  }
+
+  private async chatLoop(state: ChatState) {
+    let [chatResponse, context] = await this.chat.ask(state);
 
     if (chatResponse.toolCalls) {
       if (chatResponse.toolCalls.length != 1) {
@@ -24,15 +31,25 @@ export class AutomateService {
       }
 
       let call = chatResponse.toolCalls[0];
-      this.handleToolCall(call);
+      let toolResultText = await this.handleToolCall(call);
+
+      state.push({ role: "tool", content: toolResultText, name: call.function.name, toolCallId: call.id });
+      await this.chatLoop(state);
     } else {
       console.log(`Got text response: ${chatResponse.content}`);
     }
-
   }
 
-  async handleToolCall(call: ToolCall) {
+  private async handleToolCall(call: ToolCall): Promise<string> {
     console.log(`Executing command ${call.function.name} with args ${call.function.arguments.toString()}`)
-    // TODO
+    switch (call.function.name) {
+      case 'musicSearch':
+        let query = JSON.parse(call.function.arguments as string).query;
+        let tracks = await this.youtube.search(query).toPromise();
+        console.log(`YouTube search results for '${query}': ${JSON.stringify(tracks)}`);
+        return JSON.stringify(tracks)
+    }
+
+    return "error";
   }
 }
